620be8eb2e1e9ed4dafb5c95bbab9aeebe72bc75
Андрей Хухрянский <andrejhuhranskij@Andrejs-Mac-mini.local>
2021-11-11 17:14:19 +0200

    code optimizing

--- rspec_file_task/spec/file_spec.rb
+++ rspec_file_task/spec/file_spec.rb
@@ -1,39 +1,40 @@
 require_relative 'spec_helper'
 
 file_test = FileClass.new
-f_name = file_test.create
+
 
 
 RSpec.describe 'File interaction' do
 
   context 'Work with file' do
+    f_name = file_test.create_filename
 
     it 'verifies that user can create a file and write some data' do
-      file_test.writes(f_name, TEXT)
+      file_test.write_file(f_name, TEXT)
       expect(Dir.children(PATH).include?(f_name)).to be(true)
 
     end
 
     it 'verifies that user can read added data' do
-      expect(file_test.reads(f_name) == TEXT).to be(true)
+      expect(file_test.read_file(f_name) == TEXT).to be(true)
     end
 
     it 'verifies that user can rewrite a file' do
-      file_test.writes(f_name, ANOTHER_TEXT)
-      expect(file_test.reads(f_name) == TEXT).to be(false)
+      file_test.write_file(f_name, ANOTHER_TEXT)
+      expect(file_test.read_file(f_name) == TEXT).to be(false)
     end
 
     it 'veifies that user can read rewrited data' do
-      expect(file_test.reads(f_name) == ANOTHER_TEXT).to be(true)
+      expect(file_test.read_file(f_name) == ANOTHER_TEXT).to be(true)
     end
 
     it 'verifies that user can add data to the file' do
-      file_test.adds(f_name, TEXT)
-      expect(file_test.reads(f_name) == ANOTHER_TEXT + TEXT).to be(true)
+      file_test.add_data(f_name, TEXT)
+      expect(file_test.read_file(f_name) == ANOTHER_TEXT + TEXT).to be(true)
     end
 
     it 'verifies that user can delete a file' do
-      file_test.deleted(f_name)
+      file_test.delete_file(f_name)
       expect(Dir.children(PATH).include?(f_name)).to be(false)
     end
 
