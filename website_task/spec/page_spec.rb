9553e03371339048daedf1b908145311ec5fd077
Андрей Хухрянский <andrejhuhranskij@Andrejs-Mac-mini.local>
2021-11-20 16:08:36 +0200

    changes

--- website_task/spec/page_spec.rb
+++ website_task/spec/page_spec.rb
@@ -11,10 +11,21 @@ RSpec.describe('Webpage testing') do
     browser.close()
   end
   context 'Functional testing' do
-    it 'verifies that user can register a new account' do
+    xit 'verifies that user can register a new account' do
         web_pg.reg_process(TEXT)
-
         expect(web_pg.success_message.text) == 'Спасибо за регистрацию!'
+    end
+
+    it 'verifies that searchable items on the page is displayed' do
+      web_pg.item_tab.click
+      web_pg.item_box.click
+      expect(web_pg.url_include?('maxpedition')).to be(true)
+    end
+
+    it 'verifies that user can add item to the cart' do
+      web_pg.item.click
+      web_pg.buy_btn.click
+      expect(web_pg.cart_tab.dispayed? && web_pg.cart_tab_item.displayed?).to be(true)
     end
   end
 end
