86eb4fae92a1f9a820429f9722912f41f31bf9b5
Андрей Хухрянский <kennyman143@gmail.com>
2021-12-01 15:24:44 +0200

    test fixing

--- UI_test_task_1/page/main_page.rb
+++ UI_test_task_1/page/main_page.rb
@@ -76,7 +76,7 @@ class MainPage
   end
 
   def post_example
-    browser.find_element(link_text: 'Hello world!')
+    browser.find_element(xpath: '//ul/li/a[text()="Hello world!"]')
   end
 
   def comment
@@ -125,5 +125,17 @@ class MainPage
 
   def submit_btn
     browser.find_element(id: 'submit')
+  end
+
+  def page_init(page)
+    page_init = { 'banner' => page.banner, 'text banner' => page.banner_text, 'subtext banner' => page.banner_subtext,
+                  'search field' => page.search_field, 'new posts' => page.posts_recent, 'comments '=> page.comments,
+                  'archives tab' => page.archives, 'categories tab' => page.categories, 'meta bar' => page.meta,
+                  'data entry' => page.entry_data, 'meta entry' => page.entry_meta, 'title entry' => page.entry_title, 'footer bar' => page.footer}
+  end
+
+  def com_page_init(page)
+    comment_wdg = {'comment field' => page.comment_field, 'author bar' => page.author,
+                   'comments page url' => page.website_url, 'submit button' => page.submit_btn}
   end
 end
