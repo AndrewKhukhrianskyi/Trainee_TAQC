9c9221743a826a7321d1117a51a03f99adc44c7f
Андрей Хухрянский <kennyman143@gmail.com>
2021-12-07 18:19:33 +0200

    adds new parser task

---
+++ parser_task/tasks/parser_2.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+puts 'What would you like to find? '
+search_element = gets
+search_element.include?(' ') ? search_element.gsub!(' ', '+').chomp : search_element.chomp
+
+html_page = Nokogiri::HTML(URI.open("https://www.google.com/search?q=#{search_element}"))
+search_results = html_page.xpath("//div/a/h3//text()")
+# Bug - no links
+links = html_page.xpath('//div/cite')
+
+# All searchable results
+puts '--- All founded results: ---'
+puts search_results
+puts '--- Links: ---'
+puts links
