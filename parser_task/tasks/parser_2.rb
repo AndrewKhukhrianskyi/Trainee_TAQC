e90b0e5ef3730da9fe76418f57fe34095b777497
Андрей Хухрянский <kennyman143@gmail.com>
2021-12-07 18:14:24 +0200

    adds parser task 2

--- parser_task/tasks/parser_2.rb
+++ parser_task/tasks/parser_2.rb
@@ -1,21 +1,18 @@
 require 'nokogiri'
 require 'open-uri'
 require 'json'
-require 'selenium-webdriver'
 
-require_relative 'driver'
-require_relative 'parser_2_class'
+puts 'What would you like to find? '
+search_element = gets
+search_element.include?(' ') ? search_element.gsub!(' ', '+').chomp : search_element.chomp
 
-# Way to get access to the page
-browser.get('https://www.google.com/')
+html_page = Nokogiri::HTML(URI.open("https://www.google.com/search?q=#{search_element}"))
+search_results = html_page.xpath("//div/a/h3//text()")
+# Bug - no links
+links = html_page.xpath('//div/cite')
 
-parser_pg = FindPage.new
-parser_pg.find_input.send_keys('Ruby')
-parser_pg.search_btn.click
-
-html_page = Nokogiri::HTML(URI.open(browser.current_url))
-search_results = html_page.xpath('//h1')
-
-all_links = search_results.map {|text| text['text'] if text['text']}.compact
-
-browser.close()
+# All searchable results
+puts '--- All founded results: ---'
+puts search_results
+puts '--- Links: ---'
+puts links
