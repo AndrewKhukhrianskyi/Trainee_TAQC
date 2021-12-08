fa7bb28cfec8de59fb97b22c65f66973288da569
Андрей Хухрянский <kennyman143@gmail.com>
2021-12-08 16:09:17 +0200

    adds parser task 2

--- parser_task/tasks/parser_2.rb
+++ parser_task/tasks/parser_2.rb
@@ -4,15 +4,21 @@ require 'json'
 
 puts 'What would you like to find? '
 search_element = gets
+arr_links = []
+
 search_element.include?(' ') ? search_element.gsub!(' ', '+').chomp : search_element.chomp
 
 html_page = Nokogiri::HTML(URI.open("https://www.google.com/search?q=#{search_element}"))
 search_results = html_page.xpath("//div/a/h3//text()")
-# Bug - no links
-links = html_page.xpath('//cite')
 
-# All searchable results
+html_page.xpath('//a[@href]').each {|link| arr_links.push(link['href'].gsub!('/url?q=', '')) if !link['href'].include?('google') && !link['href'].include?('search') && !link['href'].include?('youtube')}
+web_page = Nokogiri::HTML(URI.open(arr_links[-3].match('^[a-z]+\W+[a-z]+\W+[a-z]+\W+[a-z]+').to_s))
+
 puts '--- All founded results: ---'
 puts search_results
+
 puts '--- Links: ---'
-puts links
+puts arr_links
+
+puts '--- Checked link ---'
+puts web_page.title
