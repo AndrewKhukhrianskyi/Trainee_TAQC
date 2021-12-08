7f391507226f8c3141c57b4abfeb5c308748493d
Андрей Хухрянский <kennyman143@gmail.com>
2021-12-08 16:14:44 +0200

    adds parser task 2

--- parser_task/tasks/parser_2.rb
+++ parser_task/tasks/parser_2.rb
@@ -11,7 +11,10 @@ search_element.include?(' ') ? search_element.gsub!(' ', '+').chomp : search_ele
 html_page = Nokogiri::HTML(URI.open("https://www.google.com/search?q=#{search_element}"))
 search_results = html_page.xpath("//div/a/h3//text()")
 
+# Adds only founded links by using google search
 html_page.xpath('//a[@href]').each {|link| arr_links.push(link['href'].gsub!('/url?q=', '')) if !link['href'].include?('google') && !link['href'].include?('search') && !link['href'].include?('youtube')}
+
+# Go to the third from the end website (task requirement) & broken link avoiding
 web_page = Nokogiri::HTML(URI.open(arr_links[-3].match('^[a-z]+\W+[a-z]+\W+[a-z]+\W+[a-z]+').to_s))
 
 puts '--- All founded results: ---'
