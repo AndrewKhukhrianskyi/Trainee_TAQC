36444f0198a40b5e1b296b8ddc0fab516ff4526b
Андрей Хухрянский <kennyman143@gmail.com>
2021-12-10 18:22:01 +0200

    parser task refactoring

--- parser_task/tasks/parser_2.rb
+++ parser_task/tasks/parser_2.rb
@@ -5,23 +5,33 @@ require 'json'
 puts 'What would you like to find? '
 search_element = gets
 arr_links = []
+filtered_links = []
+results = {}
 
 search_element.include?(' ') ? search_element.gsub!(' ', '+').chomp : search_element.chomp
 
 html_page = Nokogiri::HTML(URI.open("https://www.google.com/search?q=#{search_element}"))
-search_results = html_page.xpath("//div/a/h3//text()")
 
-# Adds only founded links by using google search
+puts html_page.css('a').map{|link| link['href']}
+
 html_page.xpath('//a[@href]').each {|link| arr_links.push(link['href'].gsub!('/url?q=', '')) if !link['href'].include?('google') && !link['href'].include?('search') && !link['href'].include?('youtube')}
 
-# Go to the third from the end website (task requirement) & broken link avoiding
-web_page = Nokogiri::HTML(URI.open(arr_links[-3].match('^[a-z]+\W+[a-z]+\W+[a-z]+\W+[a-z]+').to_s))
+arr_links.each do |link|
+  filtered_links.push(link.match('^[a-z]+\W+[a-z]+\W+[a-z]+\W+[a-z]+').to_s) if !link.is_a? NilClass
+end
 
-puts '--- All founded results: ---'
-puts search_results
+arr_id = Array.new(filtered_links.length) {|num| num + 1}
+
+for result in 0..arr_id.length - 1
+    results.store(arr_id[result], filtered_links[result])
+end
+
+
+# Go to the third from the end website (task requirement) & broken link avoiding
+web_page = Nokogiri::HTML(URI.open(filtered_links[-3]))
 
 puts '--- Links: ---'
-puts arr_links
+results.each_pair{|k,v| puts "Website number #{k} --- Link: #{v}"}
 
 puts '--- Checked link ---'
 puts web_page.title
