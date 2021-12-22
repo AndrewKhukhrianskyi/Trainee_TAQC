72e0df2f309e1c600abb7638a326b4ee77c995e4
Андрей Хухрянский <kennyman143@gmail.com>
2021-12-22 15:46:23 +0200

    Config file refactoring

--- api_task/config/config_file.rb
+++ api_task/config/config_file.rb
@@ -2,6 +2,13 @@ require 'mailslurp_client'
 require 'rspec'
 
 # Config file for the API key
-MailSlurpClient.configure do |config|
-    config.api_key['x-api-key'] = "YOUR API KEY"
+class ApiClass
+  def initialize
+    return 'NO API!' if ENV['API-KEY'].nil?
+    MailSlurpClient.configure do |config|
+      config.api_key['x-api-key'] = ENV['API-KEY'].to_sym
+    end
+  end
 end
+
+ApiClass.new
